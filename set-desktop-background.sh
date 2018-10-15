f3ac1bbddbdd6dfcd8228ff013a6f3a2b31bf8f5
themarcuswhit <marcuswhitaker@me.com>
2018-10-15 11:38:17 -0400

    Update set-desktop-background.sh

--- set-desktop-background.sh
+++ set-desktop-background.sh
@@ -1,4 +1,4 @@
 cd ~
 cd /Library/Desktop\ Pictures
-curl -O https://s3.amazonaws.com/service-desk-utilities/Steve_jobs_UX_quote.jpg
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Steve_jobs_UX_quote.jpg"'
+curl -O <URL to image>
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/<name of image>"'
