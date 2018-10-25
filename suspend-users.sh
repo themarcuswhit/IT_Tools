4c1d14b1662d13fe52d739f302fb8523c1215bb2
themarcuswhit <marcuswhitaker@me.com>
2018-10-25 17:26:19 -0400

    Update suspend-users.sh

--- suspend-users.sh
+++ suspend-users.sh
@@ -6,65 +6,85 @@
 # Date: May 24, 2018
 
 
+# Setting script to fail if you’re trying to reference a variable that hasn’t been set.
+set -u
+# Setting script to exit immediately if a command fails.
+set -e
+# Setting pipeline to fail if any of the commands in the pipeline failed.
+set -o pipefail
+
 #Setting log information
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1><path to log directory>`date '+%m-%d-%Y_%H:%M:%S'`-suspend-users.log 2>&1
+function logAction {
+    logTime=$(date "+%Y-%m-%d - %H:%M:%S:")
+    echo "$logTime" "$1" >> /Volumes/GoogleDrive/My\ Drive/IT\ Ops\ Repo/Offboarding/Offboarding\ Log/`date '+%m-%d-%Y'`-suspend-users.log
+}
 
-SCRIPT_RUN_TIME=`date '+%m-%d-%Y %H:%M:%S'`
-echo "This script was ran on $SCRIPT_RUN_TIME"
-echo "."
-echo "."
-echo "."
-echo "."
+#Setting time script was ran at for audit purposes
+  SCRIPT_RUN_TIME=`date '+%m-%d-%Y %H:%M:%S'`
+  printf -- "\033[37m offboard-gsuite-users.sh began at \033[101m $SCRIPT_RUN_TIME \033[0m\n"
+  logAction "Offboarding script initiated."
+  echo "."
+  echo "."
+  echo "."
+  echo "."
 
-  echo "Enter in the name of the account you are suspending"
-  read USERNAME
-  echo $USERNAME
+  #Retrieving username of account that needs to be suspended
+    printf -- "\033[4m Enter in the username of the account you are suspending \033[0m\n:"
+    read USERNAME
+    logAction "Offboarding Employee: $USERNAME"
 
-  echo "Enter in the Hiring Manager's name of the offboarding employee"
-  read HIRING_MANAGER
-  echo $HIRING_MANAGER
+  #Retrieving username of hiring manager for suspended employee
+    printf -- "\033[4m Enter in the Hiring Manager's username of the offboarding employee \033[0m\n:"
+    read HIRING_MANAGER
+    logAction "Hiring Manager: $HIRING_MANAGER"
+    echo "."
+    echo "."
+    echo "."
 
-  echo "Suspending $USERNAME in G Suite ..."
-  gam update user $USERNAME suspended on
+    printf -- "\033[93m Suspending $USERNAME in G Suite ... \033[0m\n"
+    gam update user $USERNAME suspended on
+    wait
+    printf -- "\033[32m Suspension completed at `date '+%m-%d-%Y %H:%M:%S'` \033[0m\n"
+    logAction "$USERNAME suspended"
+    echo "."
+    echo "."
+    echo "."
+
   wait
-  echo "Suspension completed at `date '+%m-%d-%Y %H:%M:%S'`"
-  echo "."
-  echo "."
-  echo "."
 
-wait
+    printf -- "\033[93m Deleting groups $USERNAME is a member of... \033[0m\n"
+    gam user $USERNAME delete groups
+    wait
+    printf "\033[32m Groups removed at `date '+%m-%d-%Y %H:%M:%S'` \033[0m\n"
+    logAction "Google Groups removed from $USERNAME"
+    echo "."
+    echo "."
+    echo "."
 
-  echo "Deleting groups $USERNAME is a member of..."
-  gam user $USERNAME delete groups
   wait
-  echo "Groups removed at `date '+%m-%d-%Y %H:%M:%S'`"
-  echo "."
-  echo "."
-  echo "."
 
-wait
+    printf -- "\033[93m Transferring $USERNAME's Google Drive data to $HIRING_MANAGER... \033[0m\n"
+    gam create datatransfer $USERNAME gdrive $HIRING_MANAGER privacy_level shared,private
+    wait
+    printf -- "\033[32m Google Drive transfer request completed at `date '+%m-%d-%Y %H:%M:%S'` \033[0m\n"
+    logAction "$USERNAME's Google Drive data transferred to $HIRING_MANAGER"
+    echo "."
+    echo "."
+    echo "."
 
-  echo "Transferring $USERNAME's Google Drive data to $HIRING_MANAGER (ran at `date '+%m-%d-%Y %H:%M:%S'`)"
-  gam create datatransfer $USERNAME gdrive $HIRING_MANAGER privacy_level shared,private
   wait
-  echo "Google Drive transfer request completed at `date '+%m-%d-%Y %H:%M:%S'`"
-  echo "."
-  echo "."
-  echo "."
 
-wait
+    printf -- "\033[93m Transferring $USERNAME's Google Calendar data to $HIRING_MANAGER... \033[0m\n"
+    gam create datatransfer $USERNAME calendar $HIRING_MANAGER release_resources true
+    wait
+    printf -- "\033[32m Google Calendar transfer request completed at `date '+%m-%d-%Y %H:%M:%S'` \033[0m\n"
+    logAction "$USERNAME's calendar transferred to $HIRING_MANAGER"
+    echo "."
+    echo "."
+    echo "."
 
-  echo "Transferring $USERNAME's Google Calendar data to $HIRING_MANAGER..."
-  gam create datatransfer $USERNAME calendar $HIRING_MANAGER release_resources true
   wait
-  echo "Google Calendar transfer request completed at `date '+%m-%d-%Y %H:%M:%S'`"
-  echo "."
-  echo "."
-  echo "."
 
-wait
-
-  echo "Offboarding process has completed for $USERNAME at `date '+%m-%d-%Y %H:%M:%S'`"
+    printf -- "\033[32m Offboarding process has completed for $USERNAME at `date '+%m-%d-%Y %H:%M:%S'` \033[0m\n"
+    logAction "Offboarding Script finished"
